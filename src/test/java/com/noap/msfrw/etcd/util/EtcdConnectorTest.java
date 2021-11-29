51802aadaebb19e0c8bea960394f9371bead19a2
umut <umutaskin@gmail.com>
2021-11-30 01:57:45 +0300

    a mock object and test fix in EtcdConnectorTest tests

--- src/test/java/com/noap/msfrw/etcd/util/EtcdConnectorTest.java
+++ src/test/java/com/noap/msfrw/etcd/util/EtcdConnectorTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.times;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,11 +16,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import com.noap.msfrw.etcd.util.watch.lock.EtcdWatchLock;
 
 import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
 import io.etcd.jetcd.KV;
+import io.etcd.jetcd.kv.GetResponse;
 import io.etcd.jetcd.options.GetOption;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,37 +40,42 @@ class EtcdConnectorTest {
 		assertEquals(true, connect);
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Test
 	@DisplayName("When All Key Values are Requested then Successfully Return Key Values")
-	void whenAllKeyValuesAreRequested_thenSuccessfullyRetrieve() {
+	void whenAllKeyValuesAreRequested_thenSuccessfullyRetrieve() throws ExecutionException, InterruptedException {
 		EtcdWatchLock etcdWatchLock = Mockito.mock(EtcdWatchLock.class);
 		EtcdConfigurationProperties ecp = Mockito.mock(EtcdConfigurationProperties.class);
 		Client mockClient = Mockito.mock(Client.class);
+		CompletableFuture<GetResponse> futureResponse = Mockito.mock(CompletableFuture.class);
 		EtcdConnector connector = new EtcdConnector(etcdWatchLock, ecp, Boolean.TRUE);
 		connector.setEtcdClient(mockClient);
 		KV kvmock = Mockito.mock(KV.class);
 		Mockito.when(mockClient.getKVClient()).thenReturn(kvmock);
-		Mockito.when(kvmock.get(any(ByteSequence.class), any(GetOption.class))).thenReturn(new CompletableFuture<>());
+		Mockito.when(kvmock.get(any(ByteSequence.class), any(GetOption.class))).thenReturn(futureResponse);
+		Mockito.when(futureResponse.get()).thenReturn(null);
 		Map<String, String> allKeyValues = connector.getAllKeyValues("application", "profile", "label");
 		Mockito.verify(mockClient,times(1)).getKVClient();
 		Mockito.verify(kvmock,times(1)).get(any(ByteSequence.class), any(GetOption.class));
 		assertNotNull(allKeyValues);
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Test
 	@DisplayName("When Get a Key Value is Requested then Successfully Return Key Value")
-	void whenKeyValueIsRequested_thenSuccessfullyRetrieve() {
+	void whenKeyValueIsRequested_thenSuccessfullyRetrieve() throws ExecutionException, InterruptedException {
 		EtcdWatchLock etcdWatchLock = Mockito.mock(EtcdWatchLock.class);
 		EtcdConfigurationProperties ecp = Mockito.mock(EtcdConfigurationProperties.class);
 		Client mockClient = Mockito.mock(Client.class);
+		CompletableFuture<GetResponse> futureResponse = Mockito.mock(CompletableFuture.class);
 		EtcdConnector connector = new EtcdConnector(etcdWatchLock, ecp, Boolean.TRUE);
 		connector.setEtcdClient(mockClient);
 		KV kvmock = Mockito.mock(KV.class);
 		Mockito.when(mockClient.getKVClient()).thenReturn(kvmock);
-		Mockito.when(kvmock.get(any(ByteSequence.class), any(GetOption.class))).thenReturn(new CompletableFuture<>());
-		String value = connector.getValue("AnyString");
+		Mockito.when(kvmock.get(any(ByteSequence.class), any(GetOption.class))).thenReturn(futureResponse);
+		Mockito.when(futureResponse.get()).thenReturn(null);
+		connector.getValue("AnyString");
 		Mockito.verify(mockClient,times(1)).getKVClient();
-		Mockito.verify(kvmock,times(1)).get(any(ByteSequence.class), any(GetOption.class));
-		assertNotNull(value);	
+		Mockito.verify(kvmock,times(1)).get(any(ByteSequence.class), any(GetOption.class));		
 	}
 }
